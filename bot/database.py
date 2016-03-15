bad20900ef45a43f309d3268c50c42ec713d0868
Cookie <cookie.io@openmailbox.org>
2016-03-15 12:10:33 +0100

    A badass plugin storage system ! Metaprog...

--- bot/database.py
+++ bot/database.py
@@ -1,10 +1,24 @@
 import redis
 import asyncio
 import logging
+from storage import Storage
 
 log = logging.getLogger('discord')
 
 class Db(object):
     def __init__(self, redis_url):
         self.redis_url = redis_url
-        self.redis = redis.StrictRedis.from_url(redis_url, decode_responses=True)
+        self.redis = redis.Redis.from_url(redis_url, decode_responses=True)
+
+    def get_storage(self, plugin, server):
+        namespace = "storage:{}:{}:".format(
+            plugin.__class__.__name__,
+            server.id
+        )
+        storage = Storage.from_url(
+            self.redis_url,
+            decode_responses=True,
+            namespace=namespace
+        )
+
+        return storage
---
+++ bot/storage.py
@@ -0,0 +1,69 @@
+from redis import Redis
+from redis import ConnectionPool
+from functools import wraps
+import inspect
+
+def prefixer(function, pref_type):
+    if pref_type == 'name':
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            return function(self, self.namespace + args[0], *(args[1:]), **kwargs)
+        return wrapper
+    elif pref_type == 'names':
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            args = list(map(lambda name: self.namespace + name, args))
+            return function(self, *args, **kwargs)
+        return wrapper
+    elif pref_type == 'src/dst':
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            return function(self, self.namespace + args[0], self.namespace + args[1], *(args[2:]), **kwargs)
+        return wrapper
+    elif pref_type == 'keys':
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            args = list(map(lambda name: self.namespace + name, args))
+            return function(self, *args, **kwargs)
+        return wrapper
+
+
+def prefix_methods(cls):
+    for attr in cls.__dict__:
+        method = getattr(cls, attr)
+        if callable(method):
+            args = inspect.getargspec(method)[0]
+            varargs = inspect.getargspec(method)[1]
+            if len(args)>1 and args[1] == 'name':
+                setattr(cls, attr, prefixer(method, 'name'))
+            elif varargs == 'names':
+                setattr(cls, attr, prefixer(method, 'names'))
+            elif 'src' in args and 'dst' in args:
+                setattr(cls, attr, prefixer(method, 'src/dst'))
+            elif len(args)>1 and args[1] == 'keys':
+                setattr(cls, attr, prefixer(method, 'keys'))
+    return cls
+
+@prefix_methods
+class Storage(Redis):
+    def __init__(self, *args, **kwargs):
+        self.namespace = kwargs.pop('namespace')
+        Redis.__init__(self, *args, **kwargs)
+
+    @classmethod
+    def from_url(cls, url, db=None, **kwargs):
+        namespace = kwargs.pop('namespace')
+        connection_pool = ConnectionPool.from_url(url, db=db, **kwargs)
+        return cls(connection_pool=connection_pool, namespace=namespace)
+
+    def get(self, name):
+        name = self.namespace + name
+        return Redis.get(self, name)
+
+    def set(self, *args, **kwargs):
+        args[0] = self.namespace + args[0]
+        return Redis.set(self, *args, **kwargs)
+
+    def incr(self, name, amount=1):
+        name = self.namespace + name
+        return Redis.incr(self, name, amount=1)
