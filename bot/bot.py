141cde8f27e465898d9f053ecf078541fc0aaf2c
Cookie <cookie.io@openmailbox.org>
2016-03-17 08:42:21 +0100

    Adding a MEE6_DEBUG environement variable

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,10 @@ logging.basicConfig(level=logging.INFO)
 
 token = os.getenv('MEE6_TOKEN')
 redis_url = os.getenv('REDIS_URL')
+mee6_debug = os.getenv('MEE6_DEBUG')
+
+if mee6_debug:
+    logging.basicConfig(level=logging.DEBUG)
 
 bot = Mee6(redis_url=redis_url)
 bot.run(token)
