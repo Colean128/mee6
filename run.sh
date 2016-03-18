ba4e397b93b0f22631800857be928a6ff37930c1
Cookie <cookie.io@openmailbox.org>
2016-03-18 15:03:03 +0100

    Adding some docker scripts

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker stop mee6 && docker rm mee6 && docker run --name mee6 -d --restart="always" --env-file envfile.list --link redis:redis mee6-bot && docker stop mee6-web && docker rm mee6-web && docker run -d --restart="always" --name mee6-web --link redis:redis --env-file envfile.list mee6-web
