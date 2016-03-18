a7b1d54390c60f48c4233321feafdf68eebc6263
Cookie <cookie.io@openmailbox.org>
2016-03-18 16:18:54 +0100

    fixing build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-git pull cd bot/ && docker build -t mee6-bot . && cd ../website && docker build -t mee6-web . && cd ../
+git pull && cd bot/ && docker build -t mee6-bot . && cd ../website && docker build -t mee6-web . && cd ../
